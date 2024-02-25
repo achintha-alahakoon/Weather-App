17b614213cd00a2ad5beaec3beeb0a43abd187b4
achintha-alahakoon <alahako-im20002@stu.kln.ac.lk>
2024-02-25 11:23:14 +0530

    first commit

---
+++ constants/index.js
@@ -0,0 +1,21 @@
+export const apiKey = '6bb8ac0783374c1883660225232711';
+
+export const weatherImages = {
+    'Partly cloudy': require('../assets/images/partlycloudy.png'),
+    'Moderate rain': require('../assets/images/moderaterain.png'),
+    'Patchy rain possible': require('../assets/images/moderaterain.png'),
+    'Sunny': require('../assets/images/sun.png'),
+    'Light rain shower': require('../assets/images/lightrainshower.png'),
+    'Clear': require('../assets/images/sun.png'),
+    'Overcast': require('../assets/images/cloud.png'),
+    'Cloudy': require('../assets/images/cloud.png'),
+    'Light rain': require('../assets/images/lightrainshower.png'),
+    'Moderate rain at times': require('../assets/images/moderaterain.png'),
+    'Heavy rain': require('../assets/images/heavyrain.png'),
+    'Heavy rain at times': require('../assets/images/heavyrain.png'),
+    'Moderate or heavy freezing rain': require('../assets/images/heavyrain.png'),
+    'Moderate or heavy rain shower': require('../assets/images/heavyrain.png'),
+    'Moderate or heavy rain with thunder': require('../assets/images/heavyrain.png'),
+    'Mist': require('../assets/images/mist.png'),
+    'other': require('../assets/images/moderaterain.png')
+}
